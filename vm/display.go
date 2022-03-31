bcba2048e14b81ac87bd6715121c7aa3940eb451
Stuart Scott <stuart.murray.scott@gmail.com>
2022-03-31 10:02:23 -0700

    Add initial display device

---
+++ vm/display.go
@@ -0,0 +1,110 @@
+package vm
+
+import (
+	"aletheiaware.com/flamego"
+	"image"
+	"image/color"
+)
+
+const PixelBytes = 8
+
+var _ (flamego.Device) = (*Display)(nil)
+
+func NewDisplay(m flamego.Memory, o uint64, w, h int) *Display {
+	d := &Display{
+		Device: *NewDevice(m, o),
+		size:   image.Rect(0, 0, w, h),
+	}
+	d.buffer = image.NewRGBA(d.size)
+	d.AddOperation(flamego.DeviceStatus, d.Status)
+	d.AddOperation(flamego.DeviceEnable, d.Enable)
+	d.AddOperation(flamego.DeviceDisable, d.Disable)
+	d.AddOperation(flamego.DeviceWrite, d.FetchFrame)
+	d.OnMemoryRead = d.LoadFrame
+	return d
+}
+
+type Display struct {
+	Device
+	size   image.Rectangle
+	buffer *image.RGBA
+}
+
+func (d *Display) Image() image.Image {
+	return d.buffer
+}
+
+func (d *Display) Status() error {
+	// TODO write to memory
+	// Command<-Manufacturer
+	// Parameter<-Serial Number/Product ID/Hardware & Software Versions
+	// DeviceAddress<-Current State
+	// MemoryAddress
+	return nil
+}
+
+func (d *Display) Enable() error {
+	c := &color.NRGBA{
+		R: 255,
+		G: 255,
+		B: 255,
+		A: 255,
+	}
+	for x := 0; x < d.size.Dx(); x++ {
+		for y := 0; y < d.size.Dy(); y++ {
+			if x == y {
+				d.buffer.Set(x, y, c)
+			}
+		}
+	}
+	d.isBusy = false
+	d.operation = flamego.DeviceNone
+	d.SignalController()
+	return nil
+}
+
+func (d *Display) Disable() error {
+	d.isBusy = false
+	d.operation = flamego.DeviceNone
+	return nil
+}
+
+func (d *Display) FetchFrame() error {
+	// Read from memory and write to image
+	if !d.memory.IsBusy() && d.memory.IsFree() {
+		d.memoryOperation = flamego.MemoryRead
+		d.memory.Read(d.memoryAddress)
+	}
+	return nil
+}
+
+func (d *Display) LoadFrame() error {
+	// Copy from memory bus into image
+	width := uint64(d.size.Dx())
+	mb := d.memory.Bus()
+	size := mb.Size()
+	count := 0
+	for ; count < size && d.parameter > 0; count += PixelBytes {
+		index := d.deviceAddress / PixelBytes
+		x := index % width
+		y := index / width
+		c := &color.NRGBA{
+			R: mb.Read(count + 0), //Ignore lower order bits
+			G: mb.Read(count + 2), //Ignore lower order bits
+			B: mb.Read(count + 4), //Ignore lower order bits
+			A: mb.Read(count + 6), //Ignore lower order bits
+		}
+		d.buffer.Set(int(x), int(y), c)
+		d.deviceAddress += PixelBytes
+		d.memoryAddress += PixelBytes
+		d.parameter -= PixelBytes
+	}
+	if d.parameter == 0 {
+		d.isBusy = false
+		d.operation = flamego.DeviceNone
+		d.SignalController()
+	} else {
+		//panic("RAWR")
+	}
+	return nil
+}
